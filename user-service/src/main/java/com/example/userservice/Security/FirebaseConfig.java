1dde19ff0452fbe41de24e7b49c59a91c0536f37
elkhater-elkharouf <76228304+elkhater-elkharouf@users.noreply.github.com>
2024-07-27 00:34:50 +0200

    backkkk

--- user-service/src/main/java/com/example/userservice/Security/FirebaseConfig.java
+++ user-service/src/main/java/com/example/userservice/Security/FirebaseConfig.java
@@ -8,21 +8,21 @@ import org.springframework.context.annotation.Configuration;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-@Configuration
-public class FirebaseConfig {
-
-    @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount = new FileInputStream("src/user-service/src/salut-bd86d-firebase-adminsdk-trwo2-9e8bbe306d.json");
-
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            return FirebaseApp.initializeApp(options);
-        } else {
-            return FirebaseApp.getInstance();
-        }
-    }
-}
+//@Configuration
+//public class FirebaseConfig {
+//
+//    @Bean
+//    public FirebaseApp initializeFirebase() throws IOException {
+//        if (FirebaseApp.getApps().isEmpty()) {
+//            FileInputStream serviceAccount = new FileInputStream("src/user-service/src/salut-bd86d-firebase-adminsdk-trwo2-9e8bbe306d.json");
+//
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//
+//            return FirebaseApp.initializeApp(options);
+//        } else {
+//            return FirebaseApp.getInstance();
+//        }
+//    }
+//}
